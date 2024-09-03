aca0565eb9f57a4fd146798b2352c93cba3d810a
sreerag-13 <sreeragsaji41@gmail.com>
2024-09-03 23:08:57 +0530

    done

---
+++ src/components/Photosign.jsx
@@ -0,0 +1,63 @@
+import React from 'react'
+
+const Photosign = () => {
+  return (
+    
+    <div>
+        <div className="container">
+                <div className="row">
+                <h1>BLOG APP </h1>
+                <h4><center>Register</center></h4>
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12"></div>
+                    <div className="row g-3">
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-label">Name</label>
+                            <input type="text" className="form-control"/>
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-label">Email</label>
+                            <input type="text" className="form-control" name='Email'/>
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-label">Password</label>
+                            <input type="password" className="form-control" name='Password'/>
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-label">Password</label>
+                            <input type="password" className="form-control" name='Password'/>
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-label">Phone</label>
+                            <input type="password" className="form-control" name='Password'/>
+                        </div>
+                    
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <button className="btn btn-success">register</button>
+                
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <button className="btn btn-success">register</button>
+                
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <button className="btn btn-success">register</button>
+                
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <button className="btn btn-success">register</button>
+                
+                        </div>
+                        
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                    <a href="/" className="btn btn-secondary">home</a>
+                    </div>
+                   
+                        
+                    </div>
+                </div>
+            </div>
+    </div>
+  )
+}
+
+export default Photosign
