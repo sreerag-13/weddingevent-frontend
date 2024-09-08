806df0776112ffbeb08354caf4817f741cbbf2f2
sreerag-13 <sreeragsaji41@gmail.com>
2024-09-08 12:10:27 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
 import Photosign from './components/Photosign';
+import Login from './components/Login';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
 
 function App() {
   return (
-    <div>
-     <Photosign/>
-    </div>
+    <BrowserRouter>
+    <Routes>
+    <Route path="/" element={<Home/>}/>
+    <Route path="/Photosign" element={<Photosign/>}/>
+    <Route path="/Login" element={<Login/>}/>
+    
+  
+  
+  
+    </Routes>
+     
+     </BrowserRouter>
   );
 }
 
---
+++ src/components/Login.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from 'react'
+const Login = () => {
+    const[data,setData]=useState({
+        "Email":"",
+        "Password":""
+
+    })
+ const inputHandler = (event) => {
+        setData({ ...data, [event.target.name]: event.target.value })
+    }
+    const readValue = () => {
+        console.log(data)
+    }
+   
+        return (
+            <div>
+                <center><h2><b>LOGIN</b></h2></center><br />
+                <div className="container">
+                    <div className="row">
+                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                            <div className="row">
+                                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                                    <form action="" className="label-form">Email</form>
+                                    <input type="text" className="form-control" name='Email' value={data.Email} onChange={inputHandler} />
+                                </div>
+                                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                                    <form action="" className="label-form">Password</form>
+                                    <input type="password" className="form-control" name='Password' value={data.Password} onChange={inputHandler} />
+                                </div>
+                                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12"><br />
+                                    <button className="btn btn-success" onClick={readValue}>Login</button>
+                                </div>
+                                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-21 col-xxl-12">
+                    <a href="/Photosign" className="btn btn-secondary">register</a>
+                    </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+        )
+    }
+
+    export default Login
